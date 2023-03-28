e7fa8366149950180a48bd74cafcf2da016feb3c
Angela Palaszewski <delarosa.ang@gmail.com>
2023-03-27 22:45:46 -0600

    complete simple liker

--- main.js
+++ main.js
@@ -3,10 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function addHidden() {
+  const error = document.querySelector('#modal')
+  error.classList.add('hidden');
+}
+addHidden();
 
+const heartIcon = document.querySelectorAll('.like-glyph')
 
+function clickHeart(e) {
+  const heart = e.target;
+  mimicServerCall('URL')
+    .then(function(){
+      if (heart.textContent === EMPTY_HEART) {
+        heart.textContent = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.textContent = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error){
+      const modal = document.querySelector('#modal');
+      modal.className = "";
+      modal.textContent = error;
+      setTimeout(() => modal.className = "hidden", 3000);
+    });
+}
 
-
+for (const glyph of heartIcon) {
+  glyph.addEventListener('click', clickHeart);
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
